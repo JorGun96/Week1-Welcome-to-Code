10e02a299f63eab3e52e85a05b0b988498e47684
Solo16 <f4rh4n1401@gmail.com>
2024-11-26 09:40:56 +0700

    first update

---
+++ Exersice/learn.js
@@ -0,0 +1,29 @@
+// string(text), integer/number, boolean
+
+let name = "Kaede" //string
+let age = 20; //number
+let haveGF = true; //boolean
+let GPA = 3.50 //number with coma
+
+//math operation
+let num1 = 16;
+let num2 = 24;
+
+console.log(num1 + num2);
+console.log(num1 - num2);
+console.log(num1 * num2);
+console.log(num1 / num2);
+console.log(num1 % num2);
+
+//we can combine string
+
+let firstName = "Hana"
+let lastName = "Kirigiri";
+
+let fullName = firstName + " " + lastName;
+
+console.log(fullName)
+
+
+//timeline read the code
+//read from left to right and up to down
